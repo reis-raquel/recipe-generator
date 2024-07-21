29c2c4fc7d15e9162b2c8b6c2befd0b577c06834
reis-raquel <raquel.a.reis@gmail.com>
2024-07-21 15:49:18 +0100

    added Apikey, apiUrl, prompt, context and axios

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-function generatePoem(event) {
-  event.preventDefault();
-
-  let poemElement = document.querySelector("#poem");
+function displayPoem(response) {
+  console.log("poem generated");
   new Typewriter("#poem", {
-    strings: "a vida é bela",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+
+  let apiKey = "dt3bbe9160a34o69bead9ceff8624028 ";
+  let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+  let context =
+    "you are a biggest fan ever of the Game of Thrones series,  who loves to write short poems themed Game of Thrones. Your mission is to generate a FOUR LINES poem in basic HTML.Separate each line with a <br>.  Sign the poem with `SheCodes AI` at the END of the poem, inside a <strong> element. Do not put a tilte on the poem. ";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating poem");
+  console.log(`prompt:${prompt}`);
+  console.log(`context: ${context}`);
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
